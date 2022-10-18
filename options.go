954c6215c17d98350fe743ea2ef513d1046cd8a9
Adphi <philippe.adrien.nousse@gmail.com>
2022-10-18 11:20:25 +0200

    badger: add advance options support and low-memory mode
    Signed-off-by: Adphi <philippe.adrien.nousse@gmail.com>

--- options.go
+++ options.go
@@ -19,6 +19,7 @@ import (
 	"time"
 
 	"github.com/dgraph-io/badger/v2"
+	boptions "github.com/dgraph-io/badger/v2/options"
 	"google.golang.org/protobuf/types/known/fieldmaskpb"
 )
 
@@ -56,6 +57,12 @@ func WithInMemory(b bool) Option {
 	}
 }
 
+func WithBadgerOptionsFunc(fn func(opts badger.Options) badger.Options) Option {
+	return func(o *options) {
+		o.badgerOptionsFunc = fn
+	}
+}
+
 func WithLogger(l Logger) Option {
 	return func(o *options) {
 		o.logger = l
@@ -65,6 +72,12 @@ func WithLogger(l Logger) Option {
 func WithNumVersionsToKeep(n int) Option {
 	return func(o *options) {
 		o.numVersions = n
+	}
+}
+
+func WithLowMemory(b bool) Option {
+	return func(o *options) {
+		o.lowMemory = b
 	}
 }
 
@@ -94,13 +107,55 @@ type options struct {
 	numVersions               int
 	ignoreProtoRegisterErrors bool
 	registerErrHandler        func(err error) error
+	badgerOptionsFunc         func(opts badger.Options) badger.Options
+	lowMemory                 bool
 }
 
 func (o options) build() badger.Options {
-	return badger.DefaultOptions(o.path).
+	opts := badger.DefaultOptions(o.path).
 		WithInMemory(o.inMemory).
 		WithNumVersionsToKeep(o.numVersions).
 		WithLogger(o.logger)
+	// https://github.com/dgraph-io/badger/issues/1304#issuecomment-630078745
+	if o.lowMemory {
+		opts.TableLoadingMode = boptions.FileIO
+		opts.ValueLogLoadingMode = boptions.FileIO
+
+		// Memtable size. This is the actively written in-memory data structure.
+		// This value also affects the size of a transaction. The size of a transaction is 15% of the table size.
+		opts.MaxTableSize = 1 << 20
+		// To allow writes at a faster speed, we create a new memtable as soon as
+		// an existing memtable is filled up. This option determines how many
+		// memtables should be kept in memory.
+		opts.NumMemtables = 1
+
+		// This option will have a significant effect the memory. If the level is kept
+		// in-memory, read are faster but the tables will be kept in memory.
+		opts.KeepL0InMemory = false
+
+		// The NumLevelZeroTables and NumLevelZeroTableStall will not have any
+		// effect on the memory if `KeepL0InMemory` is set to false.
+		opts.NumLevelZeroTables = 1
+		opts.NumLevelZeroTablesStall = 2
+
+		// SyncWrites=false has significant effect on write performance. When sync
+		// writes is set to true, badger ensures the data is flushed to the disk after a
+		// write call. For normal usage, such a high level of consistency is not required.
+		// opts.SyncWrites = false
+
+		// LoadBloomsOnOpen=false will improve the db startup speed
+		opts.LoadBloomsOnOpen = false
+
+		// Bloom filters will be kept in memory if the following option is not set. Each
+		// bloom filter takes up 5 MB of memory. A smaller bf cache would mean that
+		// bloom filters will be evicted quickly from the cache and they will be read from
+		// the disk (which is slow) and inserted into the cache.
+		// opts.MaxBfCacheSize = 100 << 20
+	}
+	if o.badgerOptionsFunc != nil {
+		opts = o.badgerOptionsFunc(opts)
+	}
+	return opts
 }
 
 var defaultOptions = options{
