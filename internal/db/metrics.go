aa5edb108d03ad278917df57e11711a0bbf22220
Adphi <philippe.adrien.nousse@gmail.com>
2023-08-15 11:14:37 +0100

    metrics: fix v3 metrics
    Signed-off-by: Adphi <philippe.adrien.nousse@gmail.com>

--- internal/db/metrics.go
+++ internal/db/metrics.go
@@ -24,22 +24,21 @@ import (
 
 const namespace = "protodb"
 
-// Source: https://grafana.com/grafana/dashboards/9574
 var (
 	badgerExpvarCollector = collectors.NewExpvarCollector(map[string]*prometheus.Desc{
-		"badger_v2_disk_reads_total":     prometheus.NewDesc(namespace+"_"+"badger_disk_reads_total", "Disk Reads", nil, nil),
-		"badger_v2_disk_writes_total":    prometheus.NewDesc(namespace+"_"+"badger_disk_writes_total", "Disk Writes", nil, nil),
-		"badger_v2_read_bytes":           prometheus.NewDesc(namespace+"_"+"badger_read_bytes", "Read bytes", nil, nil),
-		"badger_v2_written_bytes":        prometheus.NewDesc(namespace+"_"+"badger_written_bytes", "Written bytes", nil, nil),
-		"badger_v2_lsm_level_gets_total": prometheus.NewDesc(namespace+"_"+"badger_lsm_level_gets_total", "LSM Level Gets", []string{"level"}, nil),
-		"badger_v2_lsm_bloom_hits_total": prometheus.NewDesc(namespace+"_"+"badger_lsm_bloom_hits_total", "LSM Bloom Hits", []string{"level"}, nil),
-		"badger_v2_gets_total":           prometheus.NewDesc(namespace+"_"+"badger_gets_total", "Gets", nil, nil),
-		"badger_v2_puts_total":           prometheus.NewDesc(namespace+"_"+"badger_puts_total", "Puts", nil, nil),
-		"badger_v2_blocked_puts_total":   prometheus.NewDesc(namespace+"_"+"badger_blocked_puts_total", "Blocked Puts", nil, nil),
-		"badger_v2_memtable_gets_total":  prometheus.NewDesc(namespace+"_"+"badger_memtable_gets_total", "Memtable gets", nil, nil),
-		"badger_v2_lsm_size_bytes":       prometheus.NewDesc(namespace+"_"+"badger_lsm_size_bytes", "LSM Size in bytes", []string{"database"}, nil),
-		"badger_v2_vlog_size_bytes":      prometheus.NewDesc(namespace+"_"+"badger_vlog_size_bytes", "Value Log Size in bytes", []string{"database"}, nil),
-		"badger_v2_pending_writes_total": prometheus.NewDesc(namespace+"_"+"badger_pending_writes_total", "Pending Writes", []string{"database"}, nil),
+		"badger_v3_disk_reads_total":     prometheus.NewDesc(namespace+"_"+"badger_disk_reads_total", "Disk Reads", nil, nil),
+		"badger_v3_disk_writes_total":    prometheus.NewDesc(namespace+"_"+"badger_disk_writes_total", "Disk Writes", nil, nil),
+		"badger_v3_read_bytes":           prometheus.NewDesc(namespace+"_"+"badger_read_bytes", "Read bytes", nil, nil),
+		"badger_v3_written_bytes":        prometheus.NewDesc(namespace+"_"+"badger_written_bytes", "Written bytes", nil, nil),
+		"badger_v3_lsm_level_gets_total": prometheus.NewDesc(namespace+"_"+"badger_lsm_level_gets_total", "LSM Level Gets", []string{"level"}, nil),
+		"badger_v3_lsm_bloom_hits_total": prometheus.NewDesc(namespace+"_"+"badger_lsm_bloom_hits_total", "LSM Bloom Hits", []string{"level"}, nil),
+		"badger_v3_gets_total":           prometheus.NewDesc(namespace+"_"+"badger_gets_total", "Gets", nil, nil),
+		"badger_v3_puts_total":           prometheus.NewDesc(namespace+"_"+"badger_puts_total", "Puts", nil, nil),
+		"badger_v3_blocked_puts_total":   prometheus.NewDesc(namespace+"_"+"badger_blocked_puts_total", "Blocked Puts", nil, nil),
+		"badger_v3_memtable_gets_total":  prometheus.NewDesc(namespace+"_"+"badger_memtable_gets_total", "Memtable gets", nil, nil),
+		"badger_v3_lsm_size_bytes":       prometheus.NewDesc(namespace+"_"+"badger_lsm_size_bytes", "LSM Size in bytes", []string{"database"}, nil),
+		"badger_v3_vlog_size_bytes":      prometheus.NewDesc(namespace+"_"+"badger_vlog_size_bytes", "Value Log Size in bytes", []string{"database"}, nil),
+		"badger_v3_pending_writes_total": prometheus.NewDesc(namespace+"_"+"badger_pending_writes_total", "Pending Writes", []string{"database"}, nil),
 	})
 	metrics = Metrics{
 		Get:    newOpMetrics("gets"),
